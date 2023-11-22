3b3dd818c6f82fd6b7e8709ccdf6778998e25d59
yunjianshijie <2098600899@qq.com>
2023-11-22 21:19:58 +0800

    -xiugai

--- strbuf/1.c
+++ strbuf/1.c
@@ -9,7 +9,7 @@ struct strbuf {
 };
 
 
-
+//1A
 
 void strbuf_init(struct strbuf *sb, size_t alloc);
 void strbuf_attach(struct strbuf *sb, void *str, size_t len, size_t alloc);
@@ -17,13 +17,16 @@ void strbuf_release(struct strbuf *sb);
 void strbuf_swap(struct strbuf *a, struct strbuf *b);
 char *strbuf_detach(struct strbuf *sb, size_t *sz);
 int strbuf_cmp(const struct strbuf *first, const struct strbuf *second);
+void strbuf_reset(struct strbuf *sb);
+
 
 void strbuf_init(struct strbuf *sb, size_t alloc){
-sb->alloc=alloc;
-sb->len=0;
+
 if(alloc>0){
-    sb->buf=(char*) malloc(sizeof(char)*(alloc+1));
-}    
+    sb->buf=(char *) malloc(sizeof(char)*(alloc+1));
+}
+sb->alloc=alloc;
+sb->len=0; 
 }
 
 void strbuf_attach(struct strbuf *sb, void *str, size_t len, size_t alloc){
@@ -53,6 +56,7 @@ void strbuf_swap(struct strbuf *a, struct strbuf *b){
     for(int i=0; i<ab1 ;i++)
 {
     
+    
 
 
 }
@@ -64,41 +68,54 @@ void strbuf_swap(struct strbuf *a, struct strbuf *b){
 
 char *strbuf_detach(struct strbuf *sb, size_t *sz){
 *sz=sb->alloc;
-sb->buf=NULL;
+char *h=(char *)malloc(sizeof(char)*(sb->len));
+;
+sb->len=0;
+sb->alloc=0;
 
 }
 
 int strbuf_cmp(const struct strbuf *first, const struct strbuf *second){
 
-// if(!strcmp(first->buf,second->buf)){
-//     return 0;
-// }
 int a=strlen(first->buf);
 int b=strlen(second->buf);
-for(int i=0 ;i<a+2 ;i++)
-{
-    if(*(first->buf+i)>*(second->buf+i)){
-        return *(first->buf+i)-*(second->buf+i);
-    }else if((*first->buf+i)<*(second->buf+i)){
-        return -(*first->buf+i)+*(second->buf+i);
-    }
+if(first->len>second->len) return 1;
+else if(first->len ==second->len) return 0;
+else return -1;
+// for(int i=0 ;i<a+2 ;i++)
+// {
+//     if(*(first->buf+i)>*(second->buf+i)){
+//         return *(first->buf+i)-*(second->buf+i);
+//     }else if((*first->buf+i)<*(second->buf+i)){
+//         return -(*first->buf+i)+*(second->buf+i);
+//     }
 
-}
+// }
 
 return 0;
 
 }
+void strbuf_reset(struct strbuf *sb){
 
+
+
+
+}
 
 
 int main() {
-  struct strbuf sb;
-  strbuf_init(&sb, 10);
-  strbuf_attach(&sb, "xiyou", 5, 10);
-  assert(strcmp(sb.buf, "xiyou") == 0);
-  strbuf_addstr(&sb, "linux");
-  assert(strcmp(sb.buf, "xiyoulinux") == 0);
-  strbuf_release(&sb);
+    struct strbuf sb;
+    struct strbuf sb2;
+    
+
+
+
+//      strbuf_init(&sb, 10);
+//   strbuf_attach(&sb, "xiyou", 5, 10);
+//   assert(strcmp(sb.buf, "xiyou") == 0);
+//   strbuf_addstr(&sb, "linux");
+//   assert(strcmp(sb.buf, "xiyoulinux") == 0);
+//   strbuf_release(&sb);
 
 
 }

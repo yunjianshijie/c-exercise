6aa549c62f9af56dc2ece7a2ee8d986cf84814c3
yunjianshijie <2098600899@qq.com>
2023-12-03 21:26:57 +0800

    strbuf3

--- strbuf.c
+++ strbuf.c
@@ -109,7 +109,7 @@ char *strbuf_detach(struct strbuf *sb, size_t *sz)
 char *h=(char *)malloc(sizeof(char)*(sb->len));
 sb->len=0;
 sb->alloc=0;
-return h;
+return sb->buf;
 }
 
 
@@ -173,7 +173,7 @@ while(sb->alloc-sb->len<extra){
 }
 
 void strbuf_add(struct strbuf *sb, const void *data, size_t len){
-if(data=NULL) return ;
+if(data==NULL) return ;
 char *a=(char*)data;
 while (1){
 if(sb->alloc>=len+sb->len){
@@ -186,19 +186,26 @@ if(sb->alloc>=len+sb->len){
     return ;
 }else sb->alloc*=2;
 }
+return;
 }
-
+//向 sb 追加一个字符 c
 void strbuf_addch(struct strbuf *sb, int c){
-    if(sb->alloc==sb->len) sb->alloc*=2;
-    sb->buf[sb->len-1]=c;
+if(sb->alloc<=sb->len+1){
+     sb->alloc*=2;
+     char *new_buf = (char *)realloc(sb->buf, sb->alloc * sizeof(char));
+     sb->buf=new_buf; 
+     }
+    sb->buf[sb->len]=(char)c;
+     sb->len++;
     sb->buf[sb->len]='\0';
-    sb->len++;
 }
 
 void strbuf_addstr(struct strbuf *sb, const char *s){
     int h=strlen(s);
-    while(sb->alloc <  sb->len +h){
-        sb->alloc*=2;
+    while(sb->alloc <=  sb->len +h+1){
+    sb->alloc*=2;
+    char *new_buf = (char *)realloc(sb->buf, (sb->alloc) * sizeof(char));
+    sb->buf=new_buf; 
     }
     for(int i=0 ;i<h ;i++){
     sb->buf[sb->len+i]=s[i];
@@ -210,6 +217,8 @@ void strbuf_addstr(struct strbuf *sb, const char *s){
 void strbuf_addbuf(struct strbuf *sb, const struct strbuf *sb2){
     while(sb->alloc < sb->len + sb2->len){
         sb->alloc*=2;
+    char *new_buf = (char *)realloc(sb->buf, sb->alloc * sizeof(char));
+    sb->buf=new_buf; 
     }
     for(int i=0 ;i<sb2->len ;i++){
         sb->buf[i+sb->len]=sb2->buf[i];
@@ -218,52 +227,61 @@ void strbuf_addbuf(struct strbuf *sb, const struct strbuf *sb2){
     sb->buf[sb->len]='\0';
 }
 
-
+//设置 sb 的长度 len。
 void strbuf_setlen(struct strbuf *sb, size_t len){
-    if(sb->len<=len) return ;
+    if(sb->len<=len) {sb->len=len;   return ;}
     if(sb->len>len) {
         for(int i=len ;i<sb->len ;i++){
                 sb->buf[i]='\0';
         }
+        sb->len=len;
     }
 }
 
 
 size_t strbuf_avail(const struct strbuf *sb){
-    size_t i=sb->alloc-sb->len;
+    size_t i=sb->alloc-sb->len-1;
     return i;
 }
 
 void strbuf_insert(struct strbuf *sb, size_t pos, const void *data, size_t len){
     
   // strbuf_grow(sb, len);
-    while(sb->alloc <  sb->len+len ){
+    while(sb->alloc <= sb->len+len+1 ){
         sb->alloc*=2;
-    }
-    char* a=(char*)data;
-    char h[sb->len-pos];
-    for(int i=pos; i<sb->len ;i++){
-        h[i]=sb->buf[i-pos];
-    }
-    for(int i=0;i<len ;i++){
-        sb->buf[pos+i]=a[i];
-    }
 
-    for(int i=0 ;i<  sb->alloc - sb->len - len; i++  ){
-        if(sb->buf[i+pos]='\0') break;
-        sb->buf[i+pos]='\0';
     }
-    for(int i=0 ;i<sb->len-pos;i++){
-        sb->buf[i+pos]=h[i];
-    }
+    // char* a=(char*)data;
+    // char h[sb->len-pos];
+    // for(int i=pos; i<sb->len ;i++){
+    //     h[i]=sb->buf[i-pos];
+    // }
+
+    // for(int i=0;i<len ;i++){
+    //     sb->buf[pos+i]=a[i];
+    // }
+    char h[sb->len];
+    memcpy(h,sb->buf+pos,sb->len-pos);
+
+    memcpy(sb->buf+pos,data,len);
+    sb->buf[pos+len]='\0';
+    memcpy(sb->buf+pos+len,h,strlen(h));    
+    // for(int i=0 ;i<  sb->alloc - sb->len - len; i++  ){
+    //     if(sb->buf[i+pos]=='\0') break;
+    //     sb->buf[i+pos]='\0';
+    // }
+    // for(int i=0 ;i<sb->len-pos;i++){
+    //     sb->buf[i+pos]=h[i];
+    // }
     sb->buf[len+sb->len]='\0';
     sb->len+=len;
-
 }
 
 
+//c
+
 //去除 sb 缓冲区左端的所有 空格，tab, '\t'
-void strbuf_rtrim(struct strbuf *sb){
+void strbuf_ltrim(struct strbuf *sb){
 int count=0;
 while(sb->buf[count]==' ' || sb->buf[count]== '\t'){
     count++;
@@ -272,7 +290,7 @@ sb->buf=sb->buf+count;
 sb->len-=count;
 }
 
-void strbuf_ltrim(struct strbuf *sb)//去除 sb 缓冲区右端的所有 空格，tab, '\t'。
+void strbuf_rtrim(struct strbuf *sb)//去除 sb 缓冲区右端的所有 空格，tab, '\t'。
 {   
     int count=0;
     while(sb->buf[sb->len-count]==' ' || sb->buf[sb->len-count]=='\t'){
@@ -306,19 +324,32 @@ return;
 
 //D
 
-//sb 增长 hint ? hint : 8192 大小， 然后将文件描述符为 fd 的所有文件内容追加到 sb 中。
+//sb 增长 'hint ? hint : 8192' 大小， 然后将文件描述符为 fd 的所有文件内容追加到 sb 中。
 ssize_t strbuf_read(struct strbuf *sb, int fd, size_t hint){
-    open((char)fd,"r");
-    
-
-    
-    
-    return 1;
+    if(fd==-1){
+        return 0;
+    }
+    sb->alloc+=hint ?hint : 8192;
+    char *ch;
+    size_t len;
+    while( len=read(fd,ch,hint)){
+    strbuf_add(sb,ch,len);
+    }
+    ssize_t r= hint ?hint : 8192;
+    return r;
 }
 
 //将 将文件句柄为 fp 的一行内容（抛弃换行符）读取到 sb 。
 int strbuf_getline(struct strbuf *sb, FILE *fp){
-    return 1;
+    if(!feof(fp)) return 0;
+    char ch;
+    while((ch=fgetc(fp))!=EOF){
+        if(ch!='\n'){
+        strbuf_addch(sb, (int)ch);
+
+        }
+    }
+    return true;
 }
 
 
@@ -326,19 +357,51 @@ int strbuf_getline(struct strbuf *sb, FILE *fp){
 //返回 struct strbuf 的指针数组，数组的最后元素为 NULL
 struct strbuf **strbuf_split_buf(const char *str, size_t len, int terminator, int max)
 {
-    return ;
+    struct strbuf **p=(struct  strbuf**) malloc(sizeof(struct strbuf*)*max);
+for(int i=0 ;i<max;i++)
+{
+    p[i]=(struct strbuf*)malloc (sizeof(struct strbuf )*terminator);
+    }
+int count=0;
+
+for(int i=0 ;i<max ;i++){
+    p[i]->alloc=terminator;
+    int h=count;
+    for(int j=0 ;j<terminator ;i++){
+        p[i]->buf[j]=*(str+count);
+        
+        count++;
+        
+    }
+    p[i]->len=count-h;
+    }
+    return p;
+
+
 }
 
 
 //target_str : 目标字符串，str : 前缀字符串
 //，strlen : target_str 长度 ，前缀相同返回 true 失败返回 false
 bool strbuf_begin_judge(char *target_str, const char *str, int strnlen){
+    for(int i=0;i<strlen(str) ;i++){
+        if(*(str+i)!=*(target_str+i)){
+            return false;
+        }
+    }
     return true;
+
 }
 
 //target_str : 目标字符串，begin : 开始下标，end 结束下标。
 //len : target_buf的长度，参数不合法返回 NULL. 下标从0开始，[begin, end)区间。
 char *strbuf_get_mid_buf(char *target_buf, int begin, int end, int len){
-    return ;
-}
+    struct strbuf *a =(struct strbuf*)malloc(sizeof(struct strbuf));
+    a->alloc=end-begin;
+    a->len=end-begin;
+    for(int i=begin ;i<end ;i++){
+    a->buf[i]=*(target_buf+i);
+    }    
 
+    return a->buf;
+}
